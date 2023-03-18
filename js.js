d1b72fc6c9285aab097efe58529a7bdc7ca5ed92
bena013 <128201277+bena013@users.noreply.github.com>
2023-03-17 23:51:29 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,50 @@
+		// Get all menu items
+		const menuItems = document.querySelectorAll('.menu-item');
+
+		// Add event listeners for hover and click on each menu item
+		menuItems.forEach(menuItem => {
+			menuItem.addEventListener('mouseenter', () => {
+				document.getElementById('hover-sound').play();
+			});
+			menuItem.addEventListener('click', () => {
+				document.getElementById('click-sound').play();
+			});
+		});
+		
+				// Get sound icon element and audio elements
+				const soundIcon = document.getElementById('sound-icon');
+		const hoverSound = document.getElementById('hover-sound');
+		const clickSound = document.getElementById('click-sound');
+		let soundEnabled = true;
+
+		// Add event listener to sound icon
+		soundIcon.addEventListener('click', () => {
+			if (soundEnabled) {
+				soundIcon.textContent = "Sound Off";
+				hoverSound.muted = true;
+				clickSound.muted = true;
+				soundEnabled = false;
+			} else {
+				soundIcon.textContent = "Sound On";
+				hoverSound.muted = false;
+				clickSound.muted = false;
+				soundEnabled = true;
+			}
+		});
+
+		// Update sound icon when sound is enabled or disabled
+		hoverSound.addEventListener('play', () => {
+			if (soundEnabled) {
+				soundIcon.textContent = "Sound On";
+			} else {
+				soundIcon.textContent = "Sound Off";
+			}
+		});
+
+		clickSound.addEventListener('play', () => {
+			if (soundEnabled) {
+				soundIcon.textContent = "Sound On";
+			} else {
+				soundIcon.textContent = "Sound Off";
+			}
+		});
